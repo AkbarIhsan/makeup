856730ddbb506249b71935c807de5c48b1919085
AkbarIhsan <93473539+AkbarIhsan@users.noreply.github.com>
2024-07-25 09:21:51 +0700

    update index

--- FE/components/layout/navbar.vue
+++ FE/components/layout/navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <nav id="navbar" :class="['w-full h-11 sm:h-11 md:h-12 lg:h-16 fixed z-10 transition-all duration-300', { 'bg-white': isScrolled }]">
+        <nav id="navbar" :class="['w-full h-11 sm:h-11 md:h-12 lg:h-16 fixed z-10 transition-all duration-300', { 'bg-white shadow-lg': isScrolled }]">
 
                 <ul class="w-full h-full flex">
                     <li class="flex items-center ml-7 md:hidden lg:hidden">
@@ -9,20 +9,20 @@
 
                     <li class="flex h-full w-full items-center justify-center sm:justify-center md:justify-start lg:justify-start md:mx-28 lg:mx-32">
                         <div class="">
-                            <p class="text-2xl"><b class="text-cream12">WeMake</b><b class="text-white">Artisty</b></p>
+                            <p class="text-2xl text-white font-thin font-sans cursor-default"><b class="text-cream12">WeMake</b><b :class="[{'text-black' : isScrolled }]">Artisty</b></p>
                         </div>
                     </li>
                         
-                    <li class="mx-3 text-sm hidden w-auto h-full items-center md:text-md lg:text-lg text-white sm:hidden md:flex lg:flex"><a href="">Home</a></li>
+                    <li class="mx-3 text-sm hidden w-auto h-full items-center md:text-md lg:text-lg text-white sm:hidden md:flex lg:flex"><a :class="['hover:mb-2', {'text-black': isScrolled}]" href="">Home</a></li>
                     
-                    <li class="mx-3 text-sm hidden w-auto h-full items-center md:text-md lg:text-lg text-white sm:hidden md:flex lg:flex"><a href="">About</a></li>
+                    <li class="mx-3 text-sm hidden w-auto h-full items-center md:text-md lg:text-lg text-white sm:hidden md:flex lg:flex"><a :class="['hover:mb-2',{'text-black': isScrolled}]" href="">About</a></li>
                     
-                    <li class="mx-3 text-sm hidden w-auto h-full items-center md:text-md lg:text-lg text-white sm:hidden md:flex lg:flex"><a href="">MUA</a></li>
+                    <li class="mx-3 text-sm hidden w-auto h-full items-center md:text-md lg:text-lg text-white sm:hidden md:flex lg:flex"><a :class="['hover:mb-2',{'text-black': isScrolled}]" href="">MUA</a></li>
                     
-                    <li class="mx-3 text-sm hidden w-auto h-full items-center md:text-md lg:text-lg text-white sm:hidden md:flex lg:flex"><a href="">Contact</a></li>
+                    <li class="mx-3 text-sm hidden w-auto h-full items-center md:text-md lg:text-lg text-white sm:hidden md:flex lg:flex"><a :class="['hover:mb-2',{'text-black': isScrolled}]" href="">Contact</a></li>
                     
                     <li class="mx-3 sm:mr-3 md:mr-4 lg:mr-12 text-sm flex h-full w-auto items-center justify-end">
-                        <a href="login" class="h-auto w-16 sm:w-16 md:w-24 lg:w-28 rounded-md p-2 bg-gradient-to-tr from-blue-500 to-blue-300 text-white text-center "><b>Login</b></a>
+                        <a href="login" :class="['h-auto w-16 sm:w-16 md:w-24 lg:w-28 rounded-md p-2 bg-gradient-to-tr from-pink-500 to-pink-400 text-white text-center hover:scale-105 shadow-xl', {'hover:scale-105':isScrolled}]"><b>Login</b></a>
                     </li>          
                 </ul>
         </nav>
--- FE/pages/index.vue
+++ FE/pages/index.vue
@@ -5,22 +5,79 @@
 <template>
     <div>
         <LayoutNavbar />
-        <div class="place-items-center justify-center lg:justify-end md:justify-end sm:justify-center h-screen  w-full font-sans">
+        <div class="place-items-center justify-center lg:justify-center md:justify-end sm:justify-center h-screen
+                    w-full font-sans">
             <div style="background-image: url('https://i.ibb.co.com/pW3bSzk/bg.jpg');" class="w-full h-screen bg-center absolute"></div>
-            <!-- <div class="w-full h-screen absolute bg-custom-4 opacity-5"></div>
-            <div class="w-full h-screen absolute bg-custom-5 opacity-60"></div> -->
-                <div class="absolute w-full text-center sm:text-center md:text-left lg:text-left md:w-[40%] lg:w-[40%] h-full md:left-[55%] lg:left-[60%] text-custom-6 pointer-events-none">
+                <div class="absolute text-center sm:text-center md:text-left lg:text-left 
+                            w-full sm:w-full md:w-[40%] lg:w-[40%] 
+                            h-full 
+                            md:left-[55%] lg:left-[60%] 
+                            text-white ">
                     <h1 class="text-6xl mx-4 sm:text-6xl md:text-5xl lg:text-7xl sm:mx-4 md:mr-4 lg:mr-3 pt-[40%] lg:pt-[30%] md:pt-[50%]">
-                        <b class="text-white">Professional make up Studio</b>
+                        <b class="">Professional make up Studio</b>
                     </h1>
                         <h3 class="text-md mx-4 mt-[5%]">
                             creating images and characters through the medium of make-up, hairstyles and prosthetics according to a brief
                         </h3>
-                        <button class="mt-10 h-12 mx-4 w-48 rounded-md p-2 bg-custom-3 text-black hover:bg-cream12"><b>Message Us</b></button>
+                        <a class="grid mt-10 h-12 mx-4 w-48 rounded-md p-2 bg-transparent border text-white hover:scale-95 text-center items-center cursor-pointer"><b>Order Now</b></a>
+                        
                 </div>
             </div>
-            <div class="relative h-screen w-full">
+            <div class="relative flex flex-col h-screen w-full">
+                <div class="m-32">
+                    <h1>
+                        <b class="text-2xl font-semibold text-black">Best Make Up Artist </b>
+                    </h1>
+                </div>
+                <div class="mx-32 mt-10 flex flex-row gap-x-5">
+                    <a href="" class="flex justify-center hover:-mt-10
+                                w-72 h-80 
+                                shadow-2xl
+                                rounded-lg">
+                                    <div class="absolute 
+                                            w-24 h-24
+                                            -mt-12
+                                            rounded-full">
+                                            <img class="w-24 h-24 object-cover rounded-full" src="https://i.ibb.co.com/12XsfYj/aku.jpg" alt="">
+                                    </div>
+                    </a>
 
+                    <a href="" class="flex justify-center hover:-mt-10
+                                w-72 h-80 
+                                shadow-2xl
+                                rounded-lg">
+                                    <div class="border-2 border-black absolute 
+                                            w-24 h-24
+                                            -mt-12
+                                            rounded-full">
+                                            <img class="w-24 h-24 object-cover rounded-full" src="https://i.ibb.co.com/12XsfYj/aku.jpg" alt="">
+                                    </div>
+                    </a>
+
+                    <a href="" class="flex justify-center hover:-mt-10
+                                w-72 h-80 
+                                shadow-2xl
+                                rounded-lg">
+                                    <div class="border-2 border-black absolute 
+                                            w-24 h-24
+                                            -mt-12
+                                            rounded-full">
+                                            <img class="w-24 h-24 object-cover rounded-full" src="https://i.ibb.co.com/12XsfYj/aku.jpg" alt="">
+                                    </div>
+                    </a>
+
+                    <a href="" class="flex justify-center hover:-mt-10
+                                w-72 h-80 
+                                shadow-2xl
+                                rounded-lg">
+                                    <div class="border-2 border-black absolute 
+                                            w-24 h-24
+                                            -mt-12
+                                            rounded-full">
+                                            <img class="w-24 h-24 object-cover rounded-full" src="https://i.ibb.co.com/12XsfYj/aku.jpg" alt="">
+                                    </div>
+                    </a>
+                </div>
             </div>
     </div>
 </template>
