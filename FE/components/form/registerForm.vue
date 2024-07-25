03685666ac45836711e6da784ecb0555e61b866f
mutiaradevita <mutiaradevitaekaputri>
2024-07-25 10:45:24 +0700

    fix: register

--- FE/components/form/registerForm.vue
+++ FE/components/form/registerForm.vue
@@ -1,37 +1,31 @@
 <template>
-            <div class="">
-            <h1 class="text-5xl font-weight-bold m-custom-margin-1 text-cream11 [text-shadow:_0_2px_0_var(--tw-shadow-color)] shadow-cream13">Sign Up</h1>
+        <div class="">
+            <h1 class="font-thin text-center lg:text-start md:text-center mt-1 lg:mt-6 md:mt-3 sm:mt-1 text-2xl lg:text-4xl md:text-3xl sm:text-2xl">Sign Up</h1>
         </div>
         <form action="" class="m-custom-margin-2 grid gap-3 md:gap-4 mt-10">
-            <label id="inputLabelEm" class="text-start text-cream10 text-custom-font-size md:text-sm lg:text-base" style="visibility: hidden" for="inputFieldEm">Email</label>
-            <input id="inputFieldEm" class="w-full lg:h-13 md:h-12 sm:h-11 h-10 lg:text-base md:text-sm text-custom-font-size pt-0 pr-6 border-b-2 outline-none bg-transparent visible placeholder:text-cream10 placeholder:opacity-30 text-cream10" type="email" placeholder="Email" name="">
+            <label id="inputLabelEm" class="text-start text-black text-custom-font-size md:text-sm lg:text-base" style="visibility: hidden" for="inputFieldEm">Email</label>
+            <input id="inputFieldEm" class="w-full lg:h-13 md:h-12 sm:h-11 h-10 lg:text-base md:text-sm text-custom-font-size pt-0 pr-6 border-b-2 outline-none bg-transparent visible placeholder:text-black placeholder:opacity-30 text-black" type="email" placeholder="Email" name="">
 
-            <label id="inputLabelUs" class="text-start text-cream10 text-custom-font-size md:text-sm lg:text-base" style="visibility: hidden" for="inputFieldUs">Username</label>
-            <input id="inputFieldUs" class="w-full lg:h-13 md:h-12 sm:h-11 h-10 lg:text-base md:text-sm text-custom-font-size pt-0 pr-6 border-b-2 outline-none bg-transparent visible placeholder:text-cream10 placeholder:opacity-30 text-cream10" type="text" placeholder="Username" name="">
+            <label id="inputLabelUs" class="text-start text-black text-custom-font-size md:text-sm lg:text-base" style="visibility: hidden" for="inputFieldUs">Username</label>
+            <input id="inputFieldUs" class="w-full lg:h-13 md:h-12 sm:h-11 h-10 lg:text-base md:text-sm text-custom-font-size pt-0 pr-6 border-b-2 outline-none bg-transparent visible placeholder:text-black placeholder:opacity-30 text-black" type="text" placeholder="Username" name="">
 
-            <label id="inputLabelPas" class="text-start text-cream10 text-custom-font-size md:text-sm lg:text-base" style="visibility: hidden" for="inputFieldPas">Password</label>
+            <label id="inputLabelPas" class="text-start text-black text-custom-font-size md:text-sm lg:text-base" style="visibility: hidden" for="inputFieldPas">Password</label>
+
             <div class="relative w-full">
                 <div class="absolute inset-y-0 right-0 flex items-center px-2">
                     <input class="hidden js-password-toggle" id="toggle" type="checkbox" />
-                    <label class="py-1 text-sm cursor-pointer js-password-label" for="toggle"><img class="lg:h-5 md:h-4 sm:h-4 h-4" src="https://i.ibb.co.com/dD04fKs/hide-removebg-preview.png" alt=""></label>
+                    <label class="py-1 text-sm cursor-pointer js-password-label" for="toggle"><img class="h-4 lg:h-5 md:h-4 sm:h-4 w-4 lg:w-5 md:w-4 sm:w-4" src="https://i.ibb.co.com/Wp1WFhf/eye-password-see-view-svgrepo-com.png" alt=""></label>
                 </div>
-                    <input id="inputFieldPas" class="w-full lg:h-13 md:h-12 sm:h-11 h-10 lg:text-base md:text-sm text-custom-font-size pt-0 pr-6  border-b-2 outline-none bg-transparent visible placeholder:text-cream10 placeholder:opacity-30 text-cream10 js-password" placeholder="Password" type="password" autocomplete="off"/>
+                    <input id="inputFieldPas" class="w-full lg:h-13 md:h-12 sm:h-11 h-10 lg:text-base md:text-sm text-custom-font-size pt-0 pr-6 border-b-2 outline-none bg-transparent visible placeholder:text-black placeholder:opacity-30 text-black js-password" placeholder="Password" type="password" autocomplete="off"/>
             </div>
 
             <div class="flex items-center h-5">
-                <input id="terms" aria-describedby="terms" type="checkbox" class="w-3 h-3 sm:w-3 sm:h-3 md:w-3 md:h-3 lg:w-4 lg:h-4 accent-cream12 border border-cream5 rounded bg-gray-50 focus:ring-cream5 cursor-pointer">
-                <label for="terms" class="ml-2 text-custom-font-size md:text-custom-font-size lg:text-sm font-light text-cream10 cursor-pointer">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                <input id="terms" aria-describedby="terms" type="checkbox" class="w-3 h-3 sm:w-3 sm:h-3 md:w-3 md:h-3 lg:w-4 lg:h-4 accent-cream12 border border-black rounded bg-gray-50 focus:ring-red-300 cursor-pointer">
+                <label for="terms" class="ml-2 text-custom-font-size md:text-custom-font-size lg:text-sm font-light text-red-400 cursor-pointer">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
             </div>
 
-            <button class="w-[60%] lg:h-13 md:h-12 sm:h-11 h-10 mt-5 border-2 lg:text-base md:text-sm text-custom-font-size bg-cream12 text-slate-100 hover:bg-cream13 grid place-items-center font-weight-normal cursor-pointer mx-auto border-none">Sign Up</button>
+            <button class="grid place-items-center font-weight-normal cursor-pointer mx-auto border-none rounded-3xl shadow-xl font-bold border-2 bg-gradient-to-r from-red-500 to-pink-500 text-slate-100 hover:bg-red-700 hover:scale-90 mt-5 lg:text-base md:text-sm text-custom-font-size lg:h-13 md:h-12 sm:h-11 h-10 w-[40%] lg:w-[60%] md:w-[50%] sm:w[50%] ">Sign Up</button>
         </form>
-        <footer>
-            <p class="text-cream10 text-custom-font-size md:text-custom-font-size lg:text-sm">Already have an account?
-                <b class="text-cream13">Sign In
-                    <a href="login">here</a>
-                </b>
-            </p>
-        </footer>
 </template>
 
 <script>
@@ -87,10 +81,10 @@
 
         if (inputFieldPas.type === 'password') {
             inputFieldPas.type = 'text'
-            passwordLabel.innerHTML = '<img class="lg:h-5 md:h-4 sm:h-4 h-4" src="https://i.ibb.co.com/GWDTjBM/view-removebg-preview.png" alt="">'
+            passwordLabel.innerHTML = '<img class="lg:h-5 md:h-4 sm:h-4 h-4" src="https://i.ibb.co.com/B6jkXf1/eye-key-look-password-security-see-svgrepo-com.png" alt="">'
         } else {
             inputFieldPas.type = 'password'
-            passwordLabel.innerHTML = '<img class="lg:h-5 md:h-4 sm:h-4 h-4" src="https://i.ibb.co.com/dD04fKs/hide-removebg-preview.png" alt="">'
+            passwordLabel.innerHTML = '<img class="lg:h-5 md:h-4 sm:h-4 h-4" src="https://i.ibb.co.com/Wp1WFhf/eye-password-see-view-svgrepo-com.png" alt="">'
         }
 
         password.focus()
--- FE/components/layout/register/layRegister.vue
+++ FE/components/layout/register/layRegister.vue
@@ -3,12 +3,33 @@ import RegisterForm from '../../form/registerForm.vue';
 </script>
 
 <template>
-    <div style="background-image: url(https://i.ibb.co.com/FBSY8DH/4.jpg)" class="flex bg-cover sm:bg-cover md:bg-center lg:bg-center place-items-center justify-center lg:justify-end md:justify-end gap-12 m-0 h-screen px-0 py-8 font-sans w-full bg-cream6 overflow-hidden">
-        <div class="
-                relative z-10 mx-0 px-11 pt-4
-                pb-14 h-auto lg:h-screen md:h-screen sm:h-auto rounded-lg md:rounded-none lg:rounded-none bg-black text-center
-                shadow-lg bg-opacity-20 w-[80%] lg:w-[40%] md:w-[40%] sm:w-[60%] backdrop-blur-lg ">
+    <div class="flex flex-col lg:flex-row md:flex-col lg:flex-row-reverse md:flex-row-reverse sm:flex-col-reverse place-items-center justify-center h-screen font-sans w-full bg-white overflow-hidden">
+        <div class="relative mx-0 lg:px-11 md:px-9 sm:px-8 px-8 lg:pt-8 md:pt-0 pt-0 lg:mb-0 md:mb-10 sm:mb-0 mb-0 h-[80%] lg:w-[30%] md:w-[60%] sm:w-full w-full text-center shadow-2xl rounded-l-lg">
                 <RegisterForm/>
+        </div>
+        <div class="flex flex-col relative mx-0 lg:px-11 md:px-9 sm:px-6 px-6 
+                    gap-2 lg:gap-2 md:gap-2 sm:gap-2
+                    text-center bg-gradient-to-br from-red-500 to-pink-500
+                    h-[25%] lg:h-[80%] md:h-[30%] sm:h-[30%] 
+                    w-full lg:w-[30%] md:w-[60%] sm:w-full  lg:mt-0 md:mt-10 sm:mt0 mt-0
+                    shadow-lg lg:rounded-r-lg">
+                        <div class="flex h-1/2 justify-center
+                                    items-end lg:items-end md:items-center sm:items-end">
+                            <h1 class="font-bold text-white font-sans
+                                        text-2xl lg:text-5xl md:text-3xl sm:text-2xl ">
+                                Hello World!
+                            </h1>
+                        </div>
+                        <div class="h-1/2 items-center flex-col flex">
+                                <p class="font-thin text-xs text-white font-mono">Already have an account?</p>
+                                <a href="login" class="grid place-items-center border-2 rounded-3xl shadow-xl font-bold font-weight-normal cursor-pointer
+                                                            bg-transparent text-slate-100 hover:bg-white hover:text-red-500 hover:scale-90 mx-auto border-white  
+                                                            w-[20%] lg:w-[40%] md:w-[20%] sm:w-[20%] 
+                                                            mt-2 lg:mt-2 md:mt-2 sm:mt-3
+                                                            mb-0 lg:mb-0 md:mb-2 sm:mb-0
+                                                            h-10 lg:h-13 md:h-10 sm:h-11  
+                                                            lg:text-sm md:text-sm sm:text-custom-font-size text-custom-font-size ">Sign In</a>
+                        </div>
         </div>
     </div>
 </template>
